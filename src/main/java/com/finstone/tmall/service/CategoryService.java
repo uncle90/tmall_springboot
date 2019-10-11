0f83f0bdf072c2775fbdafceb48702680a968f3a
uncle90 <yuzd2016@hotmail.com>
2019-10-11 23:35:46 +0800

    Spring Data JPA + vue + thymeleaf 实现分页查询

--- src/main/java/com/finstone/tmall/service/CategoryService.java
+++ src/main/java/com/finstone/tmall/service/CategoryService.java
@@ -2,9 +2,15 @@ package com.finstone.tmall.service;
 
 import com.finstone.tmall.dao.CategoryDao;
 import com.finstone.tmall.pojo.Category;
+import com.finstone.tmall.pojo.Page4JPA;
+import org.elasticsearch.common.recycler.Recycler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,5 +23,23 @@ public class CategoryService {
     public List<Category> list(){
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         return categoryDao.findAll(sort);
+    }
+
+    /**
+     * Spring JPA 分页查询
+     * @param start 从0开始的页面索引
+     * @param size 分页页面大小
+     * @return
+     */
+    public Page4JPA<Category> list(int start, int size){
+        //排序参数
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        //分页参数
+        Pageable pageable = new PageRequest(start, size, sort);
+        //Spring JPA分页查询
+        Page page = categoryDao.findAll(pageable);
+        //包装为自定义分页 bean 返回
+        Page4JPA<Category> cs = new Page4JPA<Category>(page);
+        return cs;
     }
 }
--- src/main/java/com/finstone/tmall/web/CategoryController.java
+++ src/main/java/com/finstone/tmall/web/CategoryController.java
@@ -1,10 +1,10 @@
 package com.finstone.tmall.web;
 
 import com.finstone.tmall.pojo.Category;
+import com.finstone.tmall.pojo.Page4JPA;
 import com.finstone.tmall.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +14,16 @@ public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
-    @GetMapping(value="/categories")
+    /*@GetMapping(value="/categories")
     public List<Category> list() throws Exception{
         return categoryService.list();
+    }*/
+
+    @GetMapping(value="/categories")
+    public Page4JPA<Category> list(
+            @RequestParam(name="start", defaultValue = "0") int start, //name和value参数等价，互为别名
+            @RequestParam(name="size", defaultValue = "" + Page4JPA.defaultSize) int size)
+            throws Exception {
+        return categoryService.list(start, size);
     }
 }
