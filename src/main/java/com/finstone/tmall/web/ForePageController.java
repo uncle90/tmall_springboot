d21eec9198e52096484430ab9471afa9c9b4f16b
uncle90 <yuzd2016@hotmail.com>
2020-01-27 18:08:41 +0800

    前台功能初始化

---
+++ src/main/java/com/finstone/tmall/web/ForePageController.java
@@ -0,0 +1,10 @@
+package com.finstone.tmall.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 前台页面（买家视角）跳转
+ */
+@Controller
+public class ForePageController {
+}
---
+++ src/main/java/com/finstone/tmall/web/ForeRestController.java
@@ -0,0 +1,10 @@
+package com.finstone.tmall.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 前台页面（买家视角）涉及到的 restful 风格接口 api
+ */
+@RestController
+public class ForeRestController {
+}
