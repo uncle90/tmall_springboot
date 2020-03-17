a91cf0d78f7b43a4ec5c1dc1d21f997deef9bc6f
uncle90 <yuzd2016@hotmail.com>
2020-03-18 00:08:08 +0800

    用 Thymeleaf 组装首页

--- src/main/java/com/finstone/tmall/web/ForePageController.java
+++ src/main/java/com/finstone/tmall/web/ForePageController.java
@@ -1,10 +1,24 @@
 package com.finstone.tmall.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 前台页面（买家视角）跳转
  */
 @Controller
+@RequestMapping("")
 public class ForePageController {
+
+    /**
+     * 首页
+     * @return
+     */
+    @GetMapping("forehome")
+    public String home(){
+        return "fore/home";
+    }
+
+
 }
