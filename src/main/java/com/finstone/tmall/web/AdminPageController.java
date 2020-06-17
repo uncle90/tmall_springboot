6668716b9d3ca797fde81630eb292d2afdd0b58d
uncle90 <yuzd2016@hotmail.com>
2020-06-18 00:33:44 +0800

    后台管理页面URL采用RESTFUL风格

--- src/main/java/com/finstone/tmall/web/AdminPageController.java
+++ src/main/java/com/finstone/tmall/web/AdminPageController.java
@@ -2,68 +2,76 @@ package com.finstone.tmall.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminPageController {
 
-    @GetMapping(value="/admin")
+    //后天管理入口
+    @GetMapping(value="")
     public String admin(){
-        return "redirect:admin/category";
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping(value="/")
+    public String admin_(){
+        return "redirect:/admin/categories";
     }
 
     //分类管理
-    @GetMapping(value="admin/category")
+    @GetMapping(value="/categories")
     public String listCategory(){
         return "admin/listCategory";
     }
 
-    @GetMapping(value="/admin/category/edit")
+    @GetMapping(value="/categories/edit")
     public String editCategory(){
         return "admin/editCategory";
     }
 
     //属性管理
-    @GetMapping(value="/admin/property")
+    @GetMapping(value="/properties")
     public String listProperty(){
         return "admin/listProperty";
     }
 
-    @GetMapping(value="/admin/property/edit")
+    @GetMapping(value="/properties/edit")
     public String editProperty(){
         return "admin/editProperty";
     }
 
     //产品管理
-    @GetMapping(value ="/admin/product")
+    @GetMapping(value ="/products")
     public String listProduct(){
         return "admin/listProduct";
     }
 
-    @GetMapping(value ="/admin/product/edit")
+    @GetMapping(value ="/products/edit")
     public String editProduct(){
         return "admin/editProduct";
     }
 
     //产品图片管理
-    @GetMapping(value = "/admin/productImage/list")
+    @GetMapping(value = "/productImages")
     public String listProductImage(){
         return "admin/listProductImage";
     }
 
     //产品属性值管理
-    @GetMapping(value = "admin/propertyValue/edit")
+    @GetMapping(value = "/propertyValues/edit")
     public String listPropertyValue(){
         return "admin/editPropertyValue";
     }
 
     //用户管理
-    @GetMapping(value = "admin/user")
+    @GetMapping(value = "/users")
     public String listUser(){
         return "admin/listUser";
     }
 
     //订单管理
-    @GetMapping(value = "admin/orders")
+    @GetMapping(value = "/orders")
     public String listOrder(){
         return "admin/listOrder";
     }
